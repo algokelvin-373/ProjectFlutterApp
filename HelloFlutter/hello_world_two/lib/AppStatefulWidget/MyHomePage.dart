e674ecfba6c8056bdeb8ae0b0f538cf573fb212c
kelvintandrio <kelvin.tandrio@tsmid.com>
2020-06-20 22:15:41 +0700

    Delete AppBar and FloatingButton

--- HelloFlutter/hello_world_two/lib/AppStatefulWidget/MyHomePage.dart
+++ HelloFlutter/hello_world_two/lib/AppStatefulWidget/MyHomePage.dart
@@ -20,9 +20,6 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
@@ -36,12 +33,7 @@ class _MyHomePageState extends State<MyHomePage> {
             ),
           ],
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      )
     );
   }
 }
