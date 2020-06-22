9e5de70edc8c6a907c37819ea8470500f1c1d053
kelvintandrio <kelvin.tandrio@tsmid.com>
2020-06-22 11:02:05 +0700

    Create Folder AppStatelessWidget and AppStatefulWidget

--- HelloFlutter/hello_world_three/test/widget_test.dart
+++ HelloFlutter/hello_world_three/test/widget_test.dart
@@ -7,8 +7,7 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-
-import 'package:helloworldthree/main.dart';
+import 'package:helloworldthree/AppStatelessWidget/MyApp.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
