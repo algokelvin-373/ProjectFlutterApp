bb23539e511bf2970d4f29f5b8e8cdc71e066c1d
algokelvin-373 <kelvinht373@gmail.com>
2022-04-14 12:52:44 +0700

    Create [Flutter][07][08][09]

---
+++ FlutterContent/flutter_07_button_action_count/android/app/src/main/kotlin/algokelvin/app/example/flutter_07_button_action_count/MainActivity.kt
@@ -0,0 +1,6 @@
+package algokelvin.app.example.flutter_07_button_action_count
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ FlutterContent/flutter_09_marginpadding_only/android/app/src/main/kotlin/algokelvin/app/example/flutter_09_marginpadding_only/MainActivity.kt
@@ -0,0 +1,6 @@
+package algokelvin.app.example.flutter_09_marginpadding_only
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
