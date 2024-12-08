cf8db5ac4a9c414d53ce3753a470f281d4918681
algokelvin-373 <kelvinht373@gmail.com>
2024-12-08 10:46:12 +0700

    [Restaurant App Level 1] Develop Integration Test - Skenario Open App

--- restaurant_app_level_one/integration_test/app_test.dart
+++ restaurant_app_level_one/integration_test/app_test.dart
@@ -1,6 +1,13 @@
 import 'package:flutter_test/flutter_test.dart';
 import 'package:integration_test/integration_test.dart';
+import 'package:provider/provider.dart';
+import 'package:restaurant_app_level_one/data/api/api_services.dart';
 import 'package:restaurant_app_level_one/main.dart';
+import 'package:restaurant_app_level_one/provider/home/restaurant_list_provider.dart';
+import 'package:restaurant_app_level_one/provider/home/restaurant_search_provider.dart';
+import 'package:restaurant_app_level_one/provider/main/index_nav_provider.dart';
+import 'package:restaurant_app_level_one/provider/notification/notification_provider.dart';
+import 'package:restaurant_app_level_one/provider/theme/theme_provider.dart';
 
 import 'robot/evaluate_robot.dart';
 
@@ -9,9 +16,21 @@ void main() {
 
   testWidgets("integrate all feature", (tester) async {
     final evaluateRobot = EvaluateRobot(tester);
+    final apiServices = ApiServices();
 
-    // load ui
-    await evaluateRobot.loadUI(const RestaurantApp(isDarkMode: true));
+    await evaluateRobot.loadUI(
+      MultiProvider(
+        providers: [
+          ChangeNotifierProvider(create: (_) => ThemeProvider()),
+          ChangeNotifierProvider(create: (_) => IndexNavProvider()),
+          ChangeNotifierProvider(create: (_) => NotificationProvider()),
+          ChangeNotifierProvider(create: (_) => RestaurantListProvider(apiServices)),
+          ChangeNotifierProvider(create: (_) => RestaurantSearchProvider(apiServices)),
+        ],
+        child: const RestaurantApp(isDarkMode: true), // Widget utama yang diuji
+      ),
+    );
 
+    await tester.pumpAndSettle();
   });
 }
--- restaurant_app_level_one/integration_test/robot/evaluate_robot.dart
+++ restaurant_app_level_one/integration_test/robot/evaluate_robot.dart
@@ -13,23 +13,4 @@ class EvaluateRobot {
   Future<void> loadUI(Widget widget) async {
     await tester.pumpWidget(widget);
   }
-
-  Future<void> typeFormula(String formula) async {
-    final formulaFieldFinder = find.byKey(formulaFieldKey);
-    await tester.tap(formulaFieldFinder);
-    await tester.enterText(formulaFieldFinder, formula);
-    await tester.testTextInput.receiveAction(TextInputAction.done);
-  }
-
-  Future<void> tapButton() async {
-    final actionButtonFinder = find.byKey(actionButtonKey);
-    await tester.tap(actionButtonFinder);
-    await tester.pump();
-  }
-
-  Future<void> checkResultText(String result) async {
-    final resultFidner = find.byKey(resultKey);
-    final resultWidget = tester.widget<Text>(resultFidner);
-    expect(resultWidget.data, result);
-  }
 }
