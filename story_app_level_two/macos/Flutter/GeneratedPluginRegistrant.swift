bf77a280449bea4964220575352f33908e0a03c3
algokelvin-373 <kelvinht373@gmail.com>
2024-12-11 02:03:42 +0700

    [Story App Level 2] Setting Env 3 - Add Library

--- story_app_level_two/macos/Flutter/GeneratedPluginRegistrant.swift
+++ story_app_level_two/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,6 +5,12 @@
 import FlutterMacOS
 import Foundation
 
+import flutter_timezone
+import shared_preferences_foundation
+import sqflite_darwin
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  FlutterTimezonePlugin.register(with: registry.registrar(forPlugin: "FlutterTimezonePlugin"))
+  SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
+  SqflitePlugin.register(with: registry.registrar(forPlugin: "SqflitePlugin"))
 }
