1ac862db01015d5bf78eb5449bcbef52ecd770cb
algokelvin-373 <kelvinht373@gmail.com>
2024-12-14 17:33:49 +0700

    [Story App Level 2] Implement SingleScrollView for Login Page

---
+++ story_app_level_two/lib/screen/login/login_body_screen.dart
@@ -0,0 +1,120 @@
+import 'package:flutter/material.dart';
+
+import '../../utils/global_function.dart';
+import 'button_login_process_widget.dart';
+
+class LoginBodyScreen extends StatefulWidget {
+  final Function() onLogin;
+  final Function() onRegister;
+
+  const LoginBodyScreen({
+    super.key,
+    required this.onLogin,
+    required this.onRegister,
+  });
+
+  @override
+  State<LoginBodyScreen> createState() => _LoginBodyScreenState();
+}
+
+class _LoginBodyScreenState extends State<LoginBodyScreen> {
+  final _emailController = TextEditingController();
+  final _passwordController = TextEditingController();
+  final formKey = GlobalKey<FormState>();
+
+  @override
+  void dispose() {
+    _emailController.dispose();
+    _passwordController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Form(
+      key: formKey,
+      child: Center(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 20.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Center(
+                child: Icon(Icons.work_history, size: 60, color: Colors.blue),
+              ),
+              spaceVertical(20),
+              Center(
+                child: Text(
+                  'Sign in to your Account',
+                  style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold),
+                ),
+              ),
+              spaceVertical(10),
+              Center(
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      "Don't have an account? ",
+                      style: TextStyle(fontSize: 18),
+                    ),
+                    GestureDetector(
+                      onTap: () {
+                        widget.onRegister();
+                      },
+                      child: Text(
+                        'Sign Up',
+                        style: TextStyle(
+                          color: Colors.blue,
+                          fontSize: 18,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              spaceVertical(30),
+              TextField(
+                controller: _emailController,
+                decoration: InputDecoration(
+                  prefixIcon: Icon(Icons.email),
+                  hintText: 'Email',
+                  hintStyle: TextStyle(color: Colors.grey),
+                  filled: true,
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(10),
+                    borderSide: BorderSide.none,
+                  ),
+                ),
+              ),
+              spaceVertical(15),
+              TextField(
+                obscureText: true,
+                controller: _passwordController,
+                decoration: InputDecoration(
+                  prefixIcon: Icon(Icons.lock),
+                  hintText: 'Password',
+                  hintStyle: TextStyle(color: Colors.grey),
+                  filled: true,
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(10),
+                    borderSide: BorderSide.none,
+                  ),
+                ),
+              ),
+              spaceVertical(20),
+              ButtonLoginProcessWidget(
+                onLogin: widget.onLogin,
+                formKey: formKey,
+                emailController: _emailController,
+                passwordController: _passwordController,
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
--- story_app_level_two/lib/screen/login/login_screen.dart
+++ story_app_level_two/lib/screen/login/login_screen.dart
@@ -1,8 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:story_app_level_two/screen/login/button_login_process_widget.dart';
-import 'package:story_app_level_two/utils/global_function.dart';
+import 'package:story_app_level_two/screen/login/login_body_screen.dart';
 
-class LoginScreen extends StatefulWidget {
+class LoginScreen extends StatelessWidget {
   final Function() onLogin;
   final Function() onRegister;
 
@@ -13,109 +12,13 @@ class LoginScreen extends StatefulWidget {
   });
 
   @override
-  State<LoginScreen> createState() => _LoginScreenState();
-}
-
-class _LoginScreenState extends State<LoginScreen> {
-  final _emailController = TextEditingController();
-  final _passwordController = TextEditingController();
-  final formKey = GlobalKey<FormState>();
-
-  @override
-  void dispose() {
-    _emailController.dispose();
-    _passwordController.dispose();
-    super.dispose();
-  }
-
-  @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Form(
-        key: formKey,
-        child: Center(
-          child: Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 20.0),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Center(
-                  child: Icon(Icons.work_history, size: 60, color: Colors.blue),
-                ),
-                spaceVertical(20),
-                Center(
-                  child: Text(
-                    'Sign in to your Account',
-                    style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold),
-                  ),
-                ),
-                spaceVertical(10),
-                Center(
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Text(
-                        "Don't have an account? ",
-                        style: TextStyle(fontSize: 18),
-                      ),
-                      GestureDetector(
-                        onTap: () {
-                          widget.onRegister();
-                        },
-                        child: Text(
-                          'Sign Up',
-                          style: TextStyle(
-                            color: Colors.blue,
-                            fontSize: 18,
-                            fontWeight: FontWeight.bold,
-                          ),
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
-                spaceVertical(30),
-                TextField(
-                  controller: _emailController,
-                  decoration: InputDecoration(
-                    prefixIcon: Icon(Icons.email),
-                    hintText: 'Email',
-                    hintStyle: TextStyle(color: Colors.grey),
-                    filled: true,
-                    //fillColor: Color(0xFF1C2A35),
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(10),
-                      borderSide: BorderSide.none,
-                    ),
-                  ),
-                ),
-                spaceVertical(15),
-                TextField(
-                  obscureText: true,
-                  controller: _passwordController,
-                  decoration: InputDecoration(
-                    prefixIcon: Icon(Icons.lock),
-                    suffixIcon: Icon(Icons.visibility),
-                    hintText: 'Password',
-                    hintStyle: TextStyle(color: Colors.grey),
-                    filled: true,
-                    //fillColor: Color(0xFF1C2A35),
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(10),
-                      borderSide: BorderSide.none,
-                    ),
-                  ),
-                ),
-                spaceVertical(20),
-                ButtonLoginProcessWidget(
-                  onLogin: widget.onLogin,
-                  formKey: formKey,
-                  emailController: _emailController,
-                  passwordController: _passwordController,
-                ),
-              ],
-            ),
+      body: Center(
+        child: SingleChildScrollView(
+          child: LoginBodyScreen(
+            onLogin: onLogin,
+            onRegister: onRegister,
           ),
         ),
       ),
