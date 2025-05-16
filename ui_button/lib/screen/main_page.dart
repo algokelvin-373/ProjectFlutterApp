5ab43d55ed9aa1786bbcb4714eeee36f574abbdb
algokelvin-373 <kelvinht373@gmail.com>
2025-05-16 11:23:02 +0700

    [UI Button] Update main

---
+++ ui_button/lib/screen/main_page.dart
@@ -0,0 +1,57 @@
+import 'package:flutter/material.dart';
+
+class MainPage extends StatelessWidget {
+  const MainPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Training',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const MainPageState(title: 'Flutter Training - Button'),
+    );
+  }
+}
+
+class MainPageState extends StatefulWidget {
+  const MainPageState({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MainPageState> createState() => _MainPageState();
+}
+
+class _MainPageState extends State<MainPageState> {
+  void _showSnackBar(BuildContext context) {
+    const snackBar = SnackBar(
+      content: Text('Button ditekan!'),
+      duration: Duration(seconds: 2),
+      behavior: SnackBarBehavior.floating,
+    );
+    ScaffoldMessenger.of(context).showSnackBar(snackBar);
+  }
+
+  Widget _mainPage(BuildContext context) {
+    return Center(
+      child: ElevatedButton(
+        onPressed: () => _showSnackBar(context),
+        child: const Text('Klik Saya'),
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text(widget.title),
+      ),
+      body: _mainPage(context),
+    );
+  }
+}
