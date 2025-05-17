02eb909a7489bc071d4827a8d7a2c0df97970c2c
algokelvin-373 <kelvinht373@gmail.com>
2025-05-18 02:22:55 +0700

    Delete project

--- FlutterBeginner/flutter_04_ui_rows/android/app/src/main/kotlin/algokelvin/app/example/flutter_04_ui_rows/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package algokelvin.app.example.flutter_04_ui_rows
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- FlutterBeginner/flutter_09_marginpadding_only/android/app/src/main/kotlin/algokelvin/app/example/flutter_09_marginpadding_only/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package algokelvin.app.example.flutter_09_marginpadding_only
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
