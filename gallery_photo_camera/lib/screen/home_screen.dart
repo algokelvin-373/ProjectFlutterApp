640b1a6a37090bc211a5d2dd3f3909d6dafcf644
algokelvin-373 <kelvinht373@gmail.com>
2025-05-19 22:23:21 +0700

    [Gallery Photo Camera] Update Home Page

--- gallery_photo_camera/lib/screen/home_screen.dart
+++ gallery_photo_camera/lib/screen/home_screen.dart
@@ -18,62 +18,6 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text("Camera Project"),
-        actions: [
-          IconButton(
-            onPressed: () => _onUpload(),
-            icon:
-                context.watch<UploadProvider>().isUploading
-                    ? const CircularProgressIndicator(color: Colors.white)
-                    : const Icon(Icons.upload),
-            tooltip: "Unggah",
-          ),
-        ],
-      ),
-      body: SafeArea(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: [
-            Expanded(
-              flex: 3,
-              child:
-                  context.watch<HomeProvider>().imagePath == null
-                      ? const Align(
-                        alignment: Alignment.center,
-                        child: Icon(Icons.image, size: 100),
-                      )
-                      : _showImage(),
-            ),
-            Expanded(
-              child: Row(
-                crossAxisAlignment: CrossAxisAlignment.center,
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: [
-                  ElevatedButton(
-                    onPressed: () => _onGalleryView(),
-                    child: const Text("Gallery"),
-                  ),
-                  ElevatedButton(
-                    onPressed: () => _onCameraView(),
-                    child: const Text("Camera"),
-                  ),
-                  ElevatedButton(
-                    onPressed: () => _onCustomCameraView(),
-                    child: const Text("Custom Camera"),
-                  ),
-                ],
-              ),
-            ),
-          ],
-        ),
-      ),
-    );
-  }
-
   _onUpload() async {
     final ScaffoldMessengerState scaffoldMessengerState = ScaffoldMessenger.of(
       context,
@@ -171,5 +115,66 @@ class _HomePageState extends State<HomePage> {
     return kIsWeb
         ? Image.network(imagePath.toString(), fit: BoxFit.contain)
         : Image.file(File(imagePath.toString()), fit: BoxFit.contain);
+  }
+
+  AppBar _header() {
+    return AppBar(
+      title: const Text("Camera Project"),
+      actions: [
+        IconButton(
+          onPressed: () => _onUpload(),
+          icon:
+              context.watch<UploadProvider>().isUploading
+                  ? const CircularProgressIndicator(color: Colors.white)
+                  : const Icon(Icons.upload),
+          tooltip: "Unggah",
+        ),
+      ],
+    );
+  }
+
+  Widget _mainPage(BuildContext context) {
+    return SafeArea(
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          Expanded(
+            flex: 3,
+            child:
+                context.watch<HomeProvider>().imagePath == null
+                    ? const Align(
+                      alignment: Alignment.center,
+                      child: Icon(Icons.image, size: 100),
+                    )
+                    : _showImage(),
+          ),
+          Expanded(
+            child: Row(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                ElevatedButton(
+                  onPressed: () => _onGalleryView(),
+                  child: const Text("Gallery"),
+                ),
+                ElevatedButton(
+                  onPressed: () => _onCameraView(),
+                  child: const Text("Camera"),
+                ),
+                ElevatedButton(
+                  onPressed: () => _onCustomCameraView(),
+                  child: const Text("Custom Camera"),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(appBar: _header(), body: _mainPage(context));
   }
 }
