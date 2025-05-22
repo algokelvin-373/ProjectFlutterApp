6ab1f4d329b9561c1b23fbbc8551380ec5a292c7
algokelvin-373 <kelvinht373@gmail.com>
2025-05-22 10:06:25 +0700

    Upgrade UI ListView dan GridView

--- ui_gridview/android/app/src/main/kotlin/com/example/ui_gridview/MainActivity.kt
+++ ui_gridview/android/app/src/main/kotlin/com/example/ui_gridview/MainActivity.kt
@@ -2,5 +2,4 @@ package com.example.ui_gridview
 
 import io.flutter.embedding.android.FlutterActivity
 
-class MainActivity: FlutterActivity() {
-}
+class MainActivity : FlutterActivity()
--- ui_listview/android/app/src/main/kotlin/com/example/ui_listview/MainActivity.kt
+++ ui_listview/android/app/src/main/kotlin/com/example/ui_listview/MainActivity.kt
@@ -2,5 +2,4 @@ package com.example.ui_listview
 
 import io.flutter.embedding.android.FlutterActivity
 
-class MainActivity: FlutterActivity() {
-}
+class MainActivity : FlutterActivity()
