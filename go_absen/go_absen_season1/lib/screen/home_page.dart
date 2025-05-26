3f159ee3214ce95461a4f03f6ba5b1027be24e03
algokelvin-373 <kelvinht373@gmail.com>
2025-05-26 23:45:36 +0700

    [GoAbsen Season 1] Ready Dev

---
+++ go_absen/go_absen_season1/lib/screen/home_page.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+      ),
+      home: const HomePageState(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class HomePageState extends StatefulWidget {
+  const HomePageState({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<HomePageState> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePageState> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[],
+        ),
+      ),
+    );
+  }
+}
