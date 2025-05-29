3697988151885421718c72d068ff6c42bf8d712c
algokelvin-373 <kelvinht373@gmail.com>
2025-05-29 11:10:06 +0700

    [GoAbsen Season 1] Dev 4 - Create Item List Widget

--- go_absen/go_absen_season1/lib/widget/home_widget.dart
+++ go_absen/go_absen_season1/lib/widget/home_widget.dart
@@ -1,15 +1,22 @@
 import 'package:flutter/material.dart';
 
 class HomeWidget {
+  Widget itemListWidget(String data) {
+    return Row(
+      children: [
+        Icon(Icons.account_circle, size: 50, color: Colors.blue),
+        SizedBox(width: 5),
+        Text(data, style: TextStyle(fontSize: 20.0, color: Colors.black)),
+      ],
+    );
+  }
+
   Widget listAbsenWidget(List<String> absenList) {
     return Expanded(
       child: ListView.builder(
         itemCount: absenList.length,
         itemBuilder: (context, index) {
-          return ListTile(
-            leading: CircleAvatar(child: Text('${index + 1}')),
-            title: Text(absenList[index]),
-          );
+          return itemListWidget(absenList[index]);
         },
       ),
     );
