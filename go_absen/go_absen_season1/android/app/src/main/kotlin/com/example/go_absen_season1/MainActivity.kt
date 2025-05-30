a976ac991a56c8af4bf0e2f54e7c758f93c08dcf
algokelvin-373 <kelvinht373@gmail.com>
2025-05-30 23:56:13 +0700

    [Go Absen] Upgrade Apps - Delete Season 1 and Season 2

--- go_absen/go_absen_season1/android/app/src/main/kotlin/com/example/go_absen_season1/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.go_absen_season1
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
--- go_absen/go_absen_season1/lib/main.dart
+++
@@ -1,6 +0,0 @@
-import 'package:flutter/material.dart';
-import 'package:go_absen_season1/screen/home_page.dart';
-
-void main() {
-  runApp(const HomePage());
-}
